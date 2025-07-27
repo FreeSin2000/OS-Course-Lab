318af2e01e26f945803876ad8d262fca66baa5e5
FreeSin2000 <3474145990@qq.com>
2025-07-27 18:36:50 +0800

    Lab1 part b.

--- Lab1/kernel/arch/aarch64/boot/raspi3/peripherals/uart.c
+++ Lab1/kernel/arch/aarch64/boot/raspi3/peripherals/uart.c
@@ -140,6 +140,10 @@ static void early_uart_send(unsigned int c)
 void uart_send_string(char *str)
 {
         /* LAB 1 TODO 3 BEGIN */
+		unsigned int i;
+		for(i = 0; str[i]; i++) {
+			early_uart_send(str[i]);
+		}
         /* BLANK BEGIN */
         /* BLANK END */
         /* LAB 1 TODO 3 END */
